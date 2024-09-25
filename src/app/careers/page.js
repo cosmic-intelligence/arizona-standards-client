0f85e87e20a48c591742846a54f9201028ab7c6e
ceo <root@cosmicai.dev>
2024-09-25 12:55:39 -0700

    fix

--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -11,7 +11,7 @@ function page() {
     <PageWrapper>
       <OpenAnimation />
       <MediaHeader header="Join Our Team at Arizona Standards" body="At Arizona Standards, we believe that our people are our greatest asset. We are always looking for talented, motivated individuals who want to be a part of a growing, dynamic team. Whether you’re an experienced professional or just starting your career, we offer opportunities for development and growth in an industry that is always evolving." image={career} />
-      <ApplicationForm baseUrl={`${process.env.NEXT_PUBLIC_BASE}/application`} />
+      <ApplicationForm baseUrl={`${process.env.NEXT_PUBLIC_BASE}/api/submit/application`} />
     </PageWrapper>
   );
 }
